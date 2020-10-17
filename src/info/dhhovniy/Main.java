02b2aee920296049eae7c8eaa128652cc7c81c00
Duhovniy, Maxim <maxim.duhovniy@verint.com>
2020-10-17 10:39:13 +0300

    initial commit

---
+++ src/info/dhhovniy/Main.java
@@ -0,0 +1,91 @@
+package info.dhhovniy;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.time.Duration;
+import java.time.Instant;
+
+public class Main {
+    // author Orel Eraki
+    // Fibonacci algorithm
+    // O(log2 n)
+    public static BigInteger Fibonacci(int n) {
+
+        int num = Math.abs(n);
+        if (num == 0) {
+            return BigInteger.ZERO;
+        }
+        else if (num <= 2) {
+            return BigInteger.ONE;
+        }
+
+        BigInteger[][] number = { { BigInteger.ONE, BigInteger.ONE }, { BigInteger.ONE, BigInteger.ZERO } };
+        BigInteger[][] result = { { BigInteger.ONE, BigInteger.ONE }, { BigInteger.ONE, BigInteger.ZERO } };
+
+        while (num > 0) {
+            if (num%2 == 1) result = MultiplyMatrix(result, number);
+            number = MultiplyMatrix(number, number);
+            num/= 2;
+        }
+
+        return result[1][1].multiply(BigInteger.valueOf(((n < 0) ? -1:1)));
+    }
+
+    public static BigInteger[][] MultiplyMatrix(BigInteger[][] mat1, BigInteger[][] mat2) {
+        return new BigInteger[][] {
+                {
+                        mat1[0][0].multiply(mat2[0][0]).add(mat1[0][1].multiply(mat2[1][0])),
+                        mat1[0][0].multiply(mat2[0][1]).add(mat1[0][1].multiply(mat2[1][1]))
+                },
+                {
+                        mat1[1][0].multiply(mat2[0][0]).add(mat1[1][1].multiply(mat2[1][0])),
+                        mat1[1][0].multiply(mat2[0][1]).add(mat1[1][1].multiply(mat2[1][1]))
+                }
+        };
+    }
+
+    public static BigInteger fib(int n) {
+        BigDecimal Phi  = BigDecimal.valueOf((1 + Math.sqrt(5)) / 2);
+        BigDecimal mPhi = BigDecimal.valueOf((1 - Math.sqrt(5)) / 2);
+        return Phi.pow(n).subtract(mPhi.pow(n)).divideToIntegralValue(BigDecimal.valueOf(Math.sqrt(5))).toBigInteger();
+    }
+
+    static BigInteger fibOldSchool(int n)
+    {
+        BigInteger a = BigInteger.ZERO;
+        BigInteger b = BigInteger.ONE;
+        BigInteger c;
+        if (n == 0)
+            return a;
+        for (int i = 2; i <= n; i++)
+        {
+            c = a.add(b);
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+
+    public static void main(String[] args) {
+
+        int n = 200000;
+
+        Instant start = Instant.now();
+        String res = fib(n).toString();
+        Duration duration = Duration.between(start, Instant.now());
+        System.out.println("Fibonacci with O(1)      of " + n + "th term is " + res + " with number of digits "
+                + res.length() + " and time in nanos:" + duration.getNano());
+
+        start = Instant.now();
+        res = Fibonacci(n).toString();
+        duration = Duration.between(start, Instant.now());
+        System.out.println("Fibonacci with O(log2 N) of " + n + "th term is " + res + " with number of digits "
+                + res.length() + " and time in nanos:" + duration.getNano());
+
+        start = Instant.now();
+        res = fibOldSchool(n).toString();
+        duration = Duration.between(start, Instant.now());
+        System.out.println("Fibonacci with O(N)      of " + n + "th term is " + res + " with number of digits "
+                + res.length() + " and time in nanos:" + duration.getNano());
+    }
+}
